90a0117c9f7c8bc2771c6c66c142eef6f6ea9dad
Raphael Gimenez <contact@raphaelgimenez.fr>
2026-01-17 09:29:32 +0000

    feat(migrations): add users_roles table and enum for user roles

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260116_211935 from './20260116_211935';
+import * as migration_20260117_092924 from './20260117_092924';
 
 export const migrations = [
   {
     up: migration_20260116_211935.up,
     down: migration_20260116_211935.down,
-    name: '20260116_211935'
+    name: '20260116_211935',
+  },
+  {
+    up: migration_20260117_092924.up,
+    down: migration_20260117_092924.down,
+    name: '20260117_092924'
   },
 ];
